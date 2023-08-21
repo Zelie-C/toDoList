493993a2a4f406696e9e685f6a7a03a508d92e8f
Zelie Chemin <zelie.chemin@gmail.com>
2023-08-21 12:32:15 +0200

    ajout input, bouton, création liste

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
 import './style.css'
 
+const btnSubmit = document.querySelector("#submit") as HTMLButtonElement;
+const writtenText = document.querySelector("#writtenText") as HTMLInputElement;
+const unorderedListOfTask = document.querySelector("ul") as HTMLElement;
+
+function createTask(){
+  btnSubmit.addEventListener("click", () => {
+    let myTask = writtenText.value;
+    let listElement = document.createElement("li");
+    unorderedListOfTask.appendChild(listElement);
+    listElement.innerText = myTask;
+    
+    writtenText.value = "";
+  });
+};
+
+createTask();
