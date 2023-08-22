302a37c53a3ee9c81617879134820d445e3d0104
Zelie Chemin <zelie.chemin@gmail.com>
2023-08-22 13:40:49 +0200

    essai checkbox

--- src/main.ts
+++ src/main.ts
@@ -24,9 +24,11 @@ function createTask(){
     count++;
     const taskDiv = document.createElement("div") as HTMLDivElement;
     taskDiv.classList.add("task-div");
+    taskDiv.classList.add(`${count}`)
     toDoDiv.appendChild(taskDiv);
     const checkboxElement = document.createElement("input") as HTMLInputElement;
     checkboxElement.setAttribute("type", "checkbox");
+    checkboxElement.classList.add("checkbox");
     taskDiv.appendChild(checkboxElement);
     let listElement = document.createElement("div") as HTMLDivElement;
     listElement.setAttribute("id", `task${count}`);
@@ -35,27 +37,45 @@ function createTask(){
     taskDiv.appendChild(listElement);
     const removeTaskButton = document.createElement("button") as HTMLButtonElement;
     removeTaskButton.classList.add("remove-button");
+    removeTaskButton.setAttribute("id", `${count}`);
     removeTaskButton.innerText = "Supprimer";
     taskDiv.appendChild(removeTaskButton);
     removeTaskButton.addEventListener("click", () => {
       const parent = removeTaskButton.parentElement;
       parent?.remove();
     })
+
   });
 };
 
 createTask();
 
-
 function clearList() {
   clearButton.addEventListener("click", () => {
     myStorage.clear();
-    
+
   })
 }
 
+function isChecked(){
+  let checkboxList = document.querySelectorAll("checkbox");
+  let checkboxArray = Array.from(checkboxList);
+  checkboxArray.forEach( checkbox => checkbox.addEventListener("click", () => {
+    console.log("plouf");
+  }));
+  }
+
+isChecked();
+/*function removeTask(){
+  let buttonsList = document.querySelectorAll("remove-button");
+  let buttonsArray = Array.from(buttonsList);
+  buttonsArray.forEach( button => button.addEventListener("click", () => {
+    let parentElement = button.parentElement;
+    parentElement?.remove();
+  }))
+}*/
 
 
 /*getTask();*/
 clearList();
-removeTask();
+/*removeTask();*/
