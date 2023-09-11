f901e503fa11d400b362f0b49073b1a1538dbccf
Zelie-C <zelie.chemin@gmail.com>
2023-09-11 09:46:57 +0200

    route tache fonctionnelle, début route update

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,22 @@ import './style.css'
 const btnSubmit = document.querySelector("#submit") as HTMLButtonElement;
 const writtenText = document.querySelector("#written-text") as HTMLInputElement;
 const toDoDiv = document.querySelector(".tasks-list") as HTMLElement;
+
 // const clearButton = document.querySelector("#clear") as HTMLDivElement;
-const myStorage = window.localStorage;
+//const myStorage = window.localStorage;
 let count: number = 0;
 
-async function task(value: string) {
-  const response = await fetch(`http://localhost:3002/test/${value}`)
+async function task(value: string, taskContainer: HTMLDivElement) {
+  const response = await fetch(`http://localhost:3002/todo/${value}`)
   const message = await response.text();
-  console.log(message);
+  let taskElement = document.createElement("p") as HTMLDivElement;
+  taskContainer.appendChild(taskElement);
+  taskElement.innerText = message;
 }
 
 function createTask(){
   btnSubmit.addEventListener("click", () => {
     let myTask = writtenText.value;
-    task(myTask);
     count++;
     const taskDiv = document.createElement("div") as HTMLDivElement;
     taskDiv.classList.add("task-div");
@@ -27,15 +29,10 @@ function createTask(){
     checkboxElement.addEventListener("click", () => {
       taskDiv.classList.toggle("task-checked");
     })
-    let listElement = document.createElement("div") as HTMLDivElement;
-    listElement.setAttribute("id", `task${count}`);
-    myStorage.setItem(listElement.getAttribute("id") as string, myTask);
-    writtenText.value = " ";
-    listElement.innerText = myStorage.getItem(listElement?.getAttribute("id") as string) as string;
-    taskDiv.appendChild(listElement);
+    task(myTask, taskDiv);
+
     const removeTaskButton = document.createElement("button") as HTMLButtonElement;
     removeTaskButton.classList.add("remove-button");
-    removeTaskButton.setAttribute("id", `${count}`);
     removeTaskButton.innerText = "Supprimer";
     taskDiv.appendChild(removeTaskButton);
     removeTaskButton.addEventListener("click", () => {
