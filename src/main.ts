8d7fa197c08d3db5ad3397f03b12d85914841073
Zelie-C <zelie.chemin@gmail.com>
2023-09-11 14:29:15 +0200

    plupart des api fonctionnelles

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,69 @@ import './style.css'
 const btnSubmit = document.querySelector("#submit") as HTMLButtonElement;
 const writtenText = document.querySelector("#written-text") as HTMLInputElement;
 const toDoDiv = document.querySelector(".tasks-list") as HTMLElement;
+const deleteBtn = document.querySelector("#delete") as HTMLButtonElement;
 
 // const clearButton = document.querySelector("#clear") as HTMLDivElement;
 //const myStorage = window.localStorage;
 let count: number = 0;
 
 async function task(value: string, taskContainer: HTMLDivElement) {
-  const response = await fetch(`http://localhost:3002/todo/${value}`)
+  const response = await fetch(`http://localhost:3002/todo/${value}`, {
+    method: "POST"})
   const message = await response.text();
   let taskElement = document.createElement("p") as HTMLDivElement;
+  taskElement.setAttribute("id", `${value}`);
   taskContainer.appendChild(taskElement);
   taskElement.innerText = message;
 }
+
+async function checked(valueContainer: string, divContainer: HTMLDivElement){
+  let textElementValue = divContainer.getAttribute(valueContainer)?.valueOf();
+  const response = await fetch(`http://localhost:3002/true/${textElementValue}`, {
+    method: "PUT"})
+  const message = await response.text();
+  console.log(message);
+}
+
+async function unchecked(valueContainer: string, divContainer: HTMLDivElement){
+  let textElementValue = divContainer.getAttribute(valueContainer)?.valueOf();
+  const response = await fetch(`http://localhost:3002/false/${textElementValue}`, {
+    method: "PUT"})
+  const message = await response.text();
+  console.log(message);
+}
+
+async function deleteTask(valueContainer: string, parentDiv: HTMLDivElement){
+  let textElementValue = parentDiv.getAttribute(valueContainer)?.valueOf();
+  const response = await fetch(`http://localhost:3002/delete/${textElementValue}`, {
+    method: "DELETE"})
+  const message = await response.text();
+  console.log(message);
+}
+
+async function reloadTasks(){
+  const response = await fetch(`http://localhost:3002/getall`);
+  const message = await response.text();
+  console.log(message);
+}
+
+window.addEventListener("load", () => {
+  reloadTasks();
+})
+
+async function deleteTasks(){
+  const response = await fetch('http://localhost:3002/removeall', {
+    method: "DELETE"});
+  const message = await response.text();
+  console.log(message)
+}
+
+deleteBtn.addEventListener("click", () => {
+  deleteTasks();
+})
+
+
+
 
 function createTask(){
   btnSubmit.addEventListener("click", () => {
@@ -28,6 +79,11 @@ function createTask(){
     taskDiv.appendChild(checkboxElement);
     checkboxElement.addEventListener("click", () => {
       taskDiv.classList.toggle("task-checked");
+      if (taskDiv.classList.contains("task-checked")) {
+        checked(myTask, taskDiv)
+      } else {
+        unchecked(myTask, taskDiv)
+      }
     })
     task(myTask, taskDiv);
 
@@ -36,32 +92,13 @@ function createTask(){
     removeTaskButton.innerText = "Supprimer";
     taskDiv.appendChild(removeTaskButton);
     removeTaskButton.addEventListener("click", () => {
-      const parent = removeTaskButton.parentElement;
+      const parent = removeTaskButton.parentElement as HTMLDivElement;
+      deleteTask(myTask, parent)
       parent?.remove();
     })
 
   });
 };
 
+
 createTask();
-
-// function clearList() {
-//   clearButton.addEventListener("click", () => {
-//     myStorage.clear();
-
-//   })
-// }
-
-function isChecked(){
-  let checkboxList = document.querySelectorAll("checkbox");
-  let checkboxArray = Array.from(checkboxList);
-  checkboxArray.forEach( checkbox => checkbox.addEventListener("click", () => {
-    console.log("plouf");
-  }));
-  }
-
-isChecked();
-
-
-// clearList();
-/*removeTask();*/
